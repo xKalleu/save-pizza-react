9b2c7f11efef18e03ed2556693b977b49c95e25c
Kalleu Oliveira <xkalleu@gmail.com>
2020-07-13 22:26:07 -0300

    [Rep] Limpeza de repositorio

---
+++ system-orders/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './app'
+import * as serviceWorker from './serviceWorker'
+
+import ErrorBoundary from './error'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ErrorBoundary>
+      {(hasError) => (
+        <App hasError={hasError} />
+      )}
+    </ErrorBoundary>,
+  </React.StrictMode>,
+  document.getElementById('root')
+)
+
+serviceWorker.unregister()
