c5382152406c9656a4f903f5530d74fb2cf01ecc
Kalleu Oliveira <xkalleu@gmail.com>
2020-08-04 23:57:54 -0300

    feat: add toolbar

--- system-orders/src/pages/main/main.js
+++ system-orders/src/pages/main/main.js
@@ -1,24 +1,70 @@
-import React from 'react'
+import React, { useState, useContext } from 'react'
+import styled from 'styled-components'
 import {
   AppBar,
-  Toolbar
+  Toolbar as MaterialToolbar,
+  IconButton,
+  Typography,
+  Menu,
+  MenuItem
 } from '@material-ui/core'
 import AccountCircle from '@material-ui/icons/AccountCircle'
-import IconButton from '@material-ui/core/IconButton'
 
+import { AuthContext } from '../../contexts/auth'
 import { ReactComponent as MainLogo } from '../login/logo.svg'
 
 function MainPage () {
+  const [anchorElement, setAnchorElement] = useState(null)
+  const { logout, userInfo } = useContext(AuthContext)
+
+  const handleOpenMenu = (e) => {
+    setAnchorElement(e.target)
+  }
+
+  const handleClose = () => {
+    setAnchorElement(null)
+  }
+
+  const name = userInfo.user.displayName.split(' ')[0]
+
   return (
     <AppBar>
       <Toolbar>
-        <MainLogo />
-        <IconButton color='inherit'>
+        <LogoContainer>
+          <Logo />
+        </LogoContainer>
+
+        <Typography color='inherit'>Olá {name}</Typography>
+        <IconButton color='inherit' onClick={handleOpenMenu}>
           <AccountCircle />
         </IconButton>
+        <Menu
+          open={!!anchorElement}
+          onClose={handleClose}
+          anchorEl={anchorElement}
+        >
+          <MenuItem onClick={logout}>
+            Sair
+          </MenuItem>
+        </Menu>
       </Toolbar>
     </AppBar>
   )
 }
+
+const LogoContainer = styled.div`
+  flex-grow: 1;
+`
+
+const Logo = styled(MainLogo)`
+  width: 200px;
+  height: 50px;
+`
+
+const Toolbar = styled(MaterialToolbar)`
+  margin: 0 auto;
+  max-width: 960px;
+  width: 100%;
+`
 
 export default MainPage
